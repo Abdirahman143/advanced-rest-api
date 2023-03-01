bad0cdb5f6f1818f84e5a9d41db0b13bf20d5f0e
Abdi <KEN20963@kcbgroup.com>
2023-03-01 14:52:31 +0300

    Initial commit

---
+++ src/main/java/com/example/advancedrestapi/AdvancedRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.advancedrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedRestApiApplication.class, args);
+    }
+
+}
