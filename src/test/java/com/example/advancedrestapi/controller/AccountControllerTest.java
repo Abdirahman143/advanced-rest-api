b18d3afbc04ca58bb6db3e915f5861458160c36c
bashir <bashirabdi1994@gmail.com>
2024-01-19 10:04:33 +0300

    adding controller test class, and addAccount method test implementation

---
+++ src/test/java/com/example/advancedrestapi/controller/AccountControllerTest.java
@@ -0,0 +1,132 @@
+package com.example.advancedrestapi.controller;
+
+import com.example.advancedrestapi.customException.CustomizedExceptionHandler;
+import com.example.advancedrestapi.model.Account;
+import com.example.advancedrestapi.request.AccountRequest;
+import com.example.advancedrestapi.response.AccountResponse;
+import com.example.advancedrestapi.service.AccountService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class AccountControllerTest {
+
+    @Mock
+    private AccountService accountService;
+
+    @InjectMocks
+    private AccountController accountController;
+
+    private MockMvc mockMvc;
+    private Account account;
+    private AccountRequest accountRequest;
+    private AccountResponse accountResponse;
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp(){
+
+        CustomizedExceptionHandler customizedExceptionHandler = new CustomizedExceptionHandler();
+
+        mockMvc = MockMvcBuilders.
+                standaloneSetup(accountController).
+                setControllerAdvice(customizedExceptionHandler). //register custom exception handling
+                build();
+
+        objectMapper= new ObjectMapper();
+        account = createAccountTest();
+        accountRequest = createAccountRequestTest();
+        accountResponse = new AccountResponse();
+    }
+
+    public Account createAccountTest(){
+        // Create a date that is definitely in the past, e.g., 30 years ago from now
+        LocalDate localDateOfBirth = LocalDate.now().minusYears(30);
+        // Convert LocalDate to Date
+        Date dateOfBirth = Date.from(localDateOfBirth.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        return Account.
+                builder().
+                firstName("Abdirahman").
+                middleName("Bashir").
+                lastName("Abdi").
+                accountNumber("1234567").
+                email("bashir@test.com").
+                mobileNumber("0722345673").
+                dateOfBirth(dateOfBirth).
+                build();
+    }
+
+    private AccountRequest createAccountRequestTest(){
+        // Create a date that is definitely in the past, e.g., 30 years ago from now
+        LocalDate localDateOfBirth = LocalDate.now().minusYears(30);
+        // Convert LocalDate to Date
+        Date dateOfBirth = Date.from(localDateOfBirth.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        return AccountRequest.
+                builder().
+                firstName("Abdirahman").
+                middleName("Bashir").
+                lastName("Abdi").
+                accountNumber("1234567").
+                email("bashir@test.com").
+                mobileNumber("0722345673").
+                dateOfBirth(dateOfBirth).
+                build();
+    }
+
+    // add account details endpoint
+    @DisplayName("verify add account details should return success")
+    @Order(1)
+    @Test
+    public void addAccountDetails() throws Exception {
+        String validJson = objectMapper.writeValueAsString(accountRequest);
+        when(accountService.addAccount(any(AccountRequest.class))).thenReturn(ResponseEntity.ok(account));
+
+        mockMvc.perform(post("/api/v1/accounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(validJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accountNumber").value(account.getAccountNumber()))
+                .andExpect(jsonPath("$.email").value(account.getEmail()))
+                .andExpect(jsonPath("$.dateOfBirth").value(account.getDateOfBirth()))
+                .andDo(print());
+
+        verify(accountService, times(1)).addAccount(any(AccountRequest.class));
+    }
+
+
+}
